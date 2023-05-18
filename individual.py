20ae3b17fbc9a008334d08bb6c909bc89b9498b6
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-18 22:28:08 +0300

    Update individual.py

--- individual.py
+++ individual.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # # -*- coding: utf-8 -*-
 
+def zamik():
+    f = input('Фамилия: ')
+    n = input('Имя: ')
+
+    def make_shab(f, n):
+        shab = f'Уважаемый {f}, {n}! Вы делаете работу по замыканиям функций'
+        return shab
+
+    result = make_shab(f, n)
+    print(result)
+
+
 if __name__ == "__main__":
-    def zamik():
-        f = input('Фамилия: ')
-        n = input('Имя: ')
-        def make_shab(f,n):
-            shab = f'Уважаемый {f}, {n}! Вы делаете работу по замыканиям функций'
-            return shab
-        result = make_shab(f,n)
-        print(result)
-
-
-zamik()
+    zamik()
